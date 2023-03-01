26af92f53b13e88b5566d391a717b0fb23dbc347
dona1214 <76546326+dona1214@users.noreply.github.com>
2023-03-01 23:03:54 +0900

    Create GymSuit.java
    프로그래머스 체육복 풀이

---
+++ GymSuit.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n;
+        int []student = new int[n];
+        
+        Arrays.fill(lost, 1);
+        System.out.println(lost);
+        
+        return answer;
+    }
+}

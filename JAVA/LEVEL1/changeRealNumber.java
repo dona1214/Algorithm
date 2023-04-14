56ee85d839b75f47dabf112ab260f0f30f9aaa84
dona1214 <76546326+dona1214@users.noreply.github.com>
2023-04-14 22:25:13 +0900

    Programmer
    Change Real Number.

---
+++ JAVA/LEVEL1/changeRealNumber.java
@@ -0,0 +1,19 @@
+/*
+네오와 프로도가 숫자놀이를 하고 있습니다. 
+네오가 프로도에게 숫자를 건넬 때 일부 자릿수를 영단어로 바꾼 카드를 건네주면 프로도는 원래 숫자를 찾는 게임입니다.
+
+다음은 숫자의 일부 자릿수를 영단어로 바꾸는 예시입니다.
+
+1478 → "one4seveneight"
+234567 → "23four5six7"
+10203 → "1zerotwozero3"
+*/
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        s = s.replace("one", "1").replace("two", "2").replace("three", "3").replace("four", "4").replace("five", "5").replace("six", "6").replace("seven", "7").replace("eight", "8").replace("nine", "9").replace("zero", "0");
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}

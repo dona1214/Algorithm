b228a2ce6249d9921d56a34128a6199529ddb905
dona1214 <76546326+dona1214@users.noreply.github.com>
2023-06-03 23:42:34 +0900

    Learn Queue
    Night Shift.

---
+++ JAVA/LEVEL3/nightShift.java
@@ -0,0 +1,27 @@
+/*
+회사원 Demi는 가끔은 야근을 하는데요, 야근을 하면 야근 피로도가 쌓입니다. 
+야근 피로도는 야근을 시작한 시점에서 남은 일의 작업량을 제곱하여 더한 값입니다. 
+Demi는 N시간 동안 야근 피로도를 최소화하도록 일할 겁니다.
+Demi가 1시간 동안 작업량 1만큼을 처리할 수 있다고 할 때, 
+퇴근까지 남은 N 시간과 각 일에 대한 작업량 works에 대해 야근 피로도를 최소화한 값을 리턴하는 함수 solution을 완성해주세요.
+*/
+
+import java.util.*;
+class Solution {
+    public long solution(int n, int[] works) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int work : works) queue.offer(work);
+
+        for (int i = 0; i < n; i++) {
+            int work = queue.poll();
+            if (work == 0)
+                break;
+            else
+                queue.offer(work - 1);
+        }
+
+        long answer = 0;
+        for (int sub : queue) answer += Math.pow(sub, 2);
+            return answer;
+        }
+}
